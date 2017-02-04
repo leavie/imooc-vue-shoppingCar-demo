7c62bc1f629a9446a36ca6e390c055146fbbfbbd
leavie <gola520xxx@gmail.com>
2017-02-04 15:45:18 +0800

    convert tabs to space

--- scripts/cart.js
+++ scripts/cart.js
@@ -3,33 +3,33 @@ var vm = new Vue({
         alert(msg)
     }
     , el: '#shoppingCar'
-	, data: {
+    , data: {
         cartList: []
         , allChecked: false
-	}
-	, created: function () {
+    }
+    , created: function () {
         this.$on('onselect', this.alert); // not be applied yet
-	}
-	, mounted: function() {
-		this.$nextTick(function(){
-			this.loadCart()
-		})
-	}
-	, computed: {
+    }
+    , mounted: function() {
+        this.$nextTick(function(){
+            this.loadCart()
+        })
+    }
+    , computed: {
 
-	}
-	, methods: {
-		loadCart: function() {
+    }
+    , methods: {
+        loadCart: function() {
             var self = this;
-			this.$http.post('data/inventory.json').then(function(response) {
-				// console.log(response)
+            this.$http.post('data/inventory.json').then(function(response) {
+                // console.log(response)
                 self.cartList = response.body
-			})
-		}
+            })
+        }
         , getPrice: function (item) {
             return item.price * item.quantity
         }
-		, toggleItem: function (item, force/*optional*/) { // 切換當前商品選取狀態，或者強迫(不）選取
+        , toggleItem: function (item, force/*optional*/) { // 切換當前商品選取狀態，或者強迫(不）選取
             if (typeof item.isChecked == "undefined") {
                 this.$set(item, 'isChecked', false)
             }
@@ -38,7 +38,7 @@ var vm = new Vue({
             } else {
                 item.isChecked = force
             }
-		}
+        }
         , checkAll: function (isAllChecked) {
             this.allChecked = isAllChecked;
             this.cartList.forEach(function (item) {
@@ -57,5 +57,5 @@ var vm = new Vue({
             }, this); // this is vm
             return total;
         }
-	}
+    }
 });
