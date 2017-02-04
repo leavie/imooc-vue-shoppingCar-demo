fc67de2519b282ce92367021adb2f45e1aafe362
leavie <gola520xxx@gmail.com>
2017-02-04 15:30:15 +0800

    # add - alert - getPrice - checkAll - deleteItem - totalMoney # update # rename loadView -> loadCart # replace checkItem -> toggleItem

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,33 +1,61 @@
 var vm = new Vue({
-    el: '#shoppingCar'
+    alert: function(msg) {
+        alert(msg)
+    }
+    , el: '#shoppingCar'
 	, data: {
         cartList: []
+        , allChecked: false
 	}
 	, created: function () {
-
+        this.$on('onselect', this.alert); // not be applied yet
 	}
 	, mounted: function() {
 		this.$nextTick(function(){
-			this.loadView()
+			this.loadCart()
 		})
 	}
 	, computed: {
 
 	}
 	, methods: {
-		loadView: function() {
+		loadCart: function() {
             var self = this;
 			this.$http.post('data/inventory.json').then(function(response) {
 				// console.log(response)
                 self.cartList = response.body
 			})
 		}
-		, checkItem: function (item) {
-			if (typeof item.isChecked == "undefined") {
-				this.$set(item, 'isChecked', true)
-			} else {
-				item.isChecked = !item.isChecked;
-			 }
+        , getPrice: function (item) {
+            return item.price * item.quantity
+        }
+		, toggleItem: function (item, force/*optional*/) { // 切換當前商品選取狀態，或者強迫(不）選取
+            if (typeof item.isChecked == "undefined") {
+                this.$set(item, 'isChecked', false)
+            }
+            if (force == undefined) {
+                item.isChecked = !item.isChecked;
+            } else {
+                item.isChecked = force
+            }
 		}
+        , checkAll: function (isAllChecked) {
+            this.allChecked = isAllChecked;
+            this.cartList.forEach(function (item) {
+                this.toggleItem(item, isAllChecked)
+            }, this)
+        }
+        , deleteItem: function(item) {
+            var index = this.cartList.indexOf(item); // using id will be better
+            this.cartList.splice(index,1); // alternate in place
+        }
+        , totalMoney: function () {
+            var total = 0;
+            this.cartList.forEach(function(item){
+                if (item.isChecked)
+                    total += this.getPrice(item);
+            }, this); // this is vm
+            return total;
+        }
 	}
 });
