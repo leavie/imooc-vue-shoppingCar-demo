3a564edf7b62c4d3fd239325725082f1fff43cd6
leavie <gola520xxx@gmail.com>
2017-02-08 23:04:11 +0800

    # organize independent code related to modal-dialogue

---
+++ components/modal-dialogue/script.js
@@ -0,0 +1,21 @@
+// global component and new Vue instance mount on DOM
+Vue.component('modal-dialogue', {
+    template: '#modal-dialogue',
+    name: 'modal-dialogue',
+    props: ['title', 'message', 'yes', 'no'],
+    // components: { RoundedButton },
+    data() {
+      return {
+        msg: 'msg',
+        isShow: true,
+      }
+    },
+    methods: {
+      toogleModal() {
+        this.isShow = !this.isShow
+      },
+    }
+})
+var md = new Vue({
+    el: "#md-wrapper"
+})
---
+++ components/modal-dialogue/template.js
@@ -0,0 +1,22 @@
+  <div class="main-wrapper" v-show="isShow">
+    <div class="dialogue-cover"></div>
+    <div class="dialogue-frame">
+      <div class="dialogue">
+        <p class="title" v-text="title || 'Title'"></p>
+        <p class="message" v-text="message || 'Message'"></p>
+        <div class="action">
+          <p class="item emergency">
+            <button key="yes" v-text="yes || 'Yes'"
+            v-on:click="toogleModal"
+            v-bind:options="{state:2, size:2}" ></button>
+          </p>
+          <p class="item">
+            <button key="no"  v-text="no || 'No'"
+            v-on:click="toogleModal"
+            v-bind:options="{state:0, size:2}"></button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
