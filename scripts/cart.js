fb98eb6996e446fb27def2da9c19cf679e4729a3
leavie <gola520xxx@gmail.com>
2019-03-27 02:52:11 +0800

    refine name, logic drop redundant comment

--- scripts/cart.js
+++ scripts/cart.js
@@ -17,7 +17,6 @@ var vm = new Vue({
         , totalMoney: function () {
             var total = 0;
             this.cartList.forEach(function(item){
-                //console.log('totalMoney');
                 if (item.isChecked)
                     total += this.getPrice(item);
             }, this); // this is vm
@@ -26,37 +25,34 @@ var vm = new Vue({
     }
     , methods: {
         loadCart: function() {
-            console.log('a')
             var self = this;
             this.$http.get('data/inventory.json').then(function(response) { // cannot use post in my environment
                 self.cartList =
                 Array.isArray(response.body)
                     ? response.body
                     : JSON.parse(response.body)
-                console.log('b')
             }).catch(function(){
                 console.log('catch error')
             })
-            console.log('c')
         }
         , getPrice: function (item) {
             return item.price * item.quantity
         }
-        , checkItem: function (item, force) { // 強迫(不）選取
-            if (item.isChecked == force) return;
+        , checkItem: function (item, checkOrNot) {
+            if (item.isChecked == checkOrNot) return;
             if (typeof item.isChecked == "undefined") {
-                this.$set(item, 'isChecked', false)
+                this.$set(item, 'isChecked', checkOrNot)
+            } else {
+                item.isChecked = checkOrNot;
             }
-                item.isChecked = force;
         }
         , toggleItem: function (item) { // 切換當前商品的選取狀態
             this.checkItem(item, !item.isChecked);
         }
-        , checkAllItems: function (force) {
-            if(this.isAllChecked == force) return;
+        , checkAllItems: function (checkOrNot) {
+            if(this.isAllChecked == checkOrNot) return;
             this.cartList.forEach(function (item) {
-                //console.log('checkAllItems');
-                this.checkItem(item, force);
+                this.checkItem(item, checkOrNot);
             }, this)
         }
         ,
@@ -68,15 +64,16 @@ var vm = new Vue({
             if (index == -1) return;
             this.cartList.splice(index,1); // alternate in place
         }
-        , changeQuantity: function (item, way) {
-            var temp = item.quantity + way;
-            var reset = temp <= 0;
-            if (reset) {
+        , changeQuantity: function (item, increment) {
+            var newValue = item.quantity + increment;
+            var checkOrNot = temp > 0;
+            var resetOrNot = !checkOrNot
+            if (resetOrNot) {
                 item.quantity = 0;
             } else {
-                item.quantity = temp;
+                item.quantity = newValue;
             }
-            this.checkItem(item, !reset);
+            this.checkItem(item, checkOrNot);
         }
         , requestDeletion: function (item) {
             eventHub.$emit('request', this.deleteItem, item)
